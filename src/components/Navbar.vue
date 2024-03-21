4bb574e6297d21ba9c934faddd42d9163d9ae6d6
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-03-21 19:47:43 +0200

    Navbar css changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,14 +14,13 @@ export default {
 </script>
 
 <template>
-  <v-app-bar>
-    <v-tabs style="margin: 0 auto">
+  <v-app-bar style="margin: 0 auto">
+    <v-tabs align-tabs="center" style="margin: 0 auto">
       <v-tab @click="navigateToAbout">About</v-tab>
       <v-tab @click="navigateToProjects">Projects</v-tab>
     </v-tabs>
   </v-app-bar>
-
-  <div style="padding-top: 10%"><RouterView></RouterView></div>
+  <div class="page-content" style="padding-top: 10%"><RouterView></RouterView></div>
 </template>
 
 <style></style>
