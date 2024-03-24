74b826ae967f33bef43205e2112e2435b00e5444
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-03-24 21:43:00 +0200

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <v-app class="app">
     <Navbar></Navbar>
+    <Footer></Footer>
   </v-app>
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts">
+export default {}
+</script>
+
+<template>
+  <v-footer>
+    <div class="px-4 py-2 text-center w-100">
+      {{ new Date().getFullYear() }} — <strong>Michael Paulinus &copy;</strong>
+    </div>
+  </v-footer>
+</template>
