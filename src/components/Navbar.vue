b031b416cc3852a74749512a8f1e87044af8ce33
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-03-27 16:13:03 +0200

    Implemented v-tabs

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,12 @@
 <script lang="ts">
-import router from '@/router'
+import About from './About.vue'
+import Projects from './Projects.vue'
 
 export default {
-  methods: {
-    navigateToAbout() {
-      router.push('/')
-    },
-    navigateToProjects() {
-      router.push('/projects')
+  components: { About, Projects },
+  data() {
+    return {
+      tab: 'one'
     }
   }
 }
@@ -15,12 +14,18 @@ export default {
 
 <template>
   <v-app-bar style="margin: 0 auto">
-    <v-tabs align-tabs="center" style="margin: 0 auto">
-      <v-tab @click="navigateToAbout">About</v-tab>
-      <v-tab @click="navigateToProjects">Projects</v-tab>
+    <v-tabs v-model="tab" align-tabs="center" style="margin: 0 auto">
+      <v-tab value="one">About</v-tab>
+      <v-tab value="two">Projects</v-tab>
     </v-tabs>
   </v-app-bar>
-  <div class="page-content" style="padding-top: 10%"><RouterView></RouterView></div>
+
+  <div class="page-content" style="padding-top: 10%">
+    <v-window v-model="tab">
+      <v-window-item class="tabs" value="one"><About></About></v-window-item>
+      <v-window-item value="two"><Projects></Projects></v-window-item>
+    </v-window>
+  </div>
 </template>
 
 <style></style>
